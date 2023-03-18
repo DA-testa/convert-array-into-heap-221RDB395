bdacc75eab7e9811ae704f670a9d263c1fd60d8b
Anastasija Bondare <125151183+221RDB395@users.noreply.github.com>
2023-03-18 14:19:44 +0000

    1-2

--- build_heap.py
+++ build_heap.py
@@ -1,13 +1,23 @@
+# Anastasija Bondare 221RDB395
 # python3
 
-
 def build_heap(data):
+    n = len(data)
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
-    return swaps
+    elements_swaps = 0
+    for i in range(n // 2, -1, -1):
+        k = i
+        while 2 * k + 1 < n:
+            j = 2 * k + 1
+            if j + 1 < n and data[j + 1] < data[j]:
+                j += 1
+            if data[k] <= data[j]:
+                break
+            swaps.append((k, j))
+            data[k], data[j] = data[j], data[k]
+            k = j
+            elements_swaps += 1
+    return swaps, elements_swaps 
 
 
 def main():
@@ -16,6 +26,17 @@ def main():
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
 
+    ievade = input()
+
+    if "I" in ievade: # Ja ievadītajā tekstā ir "I", tad 
+        number = int(input()) # ievada elementu skaitu, cik  būs kokā
+        
+
+    if "F" in ievade: # Ja ievadītajā tekstā ir "F", tad 
+        ievade = "tests/" + input() # nolasa visu ievadīto ceļu līdz failam,
+        with open(ievade, 'r') as file: # nolasa faila saturu un aizver to,
+            number = int(file.readline().strip())  # pēc tam nolasa pirmo rindu un pārveido to par Integer jeb skaitli, kas norāda, cik elemntu būs kokā
+            
 
     # input from keyboard
     n = int(input())
@@ -26,11 +47,11 @@ def main():
 
     # calls function to assess the data 
     # and give back all swaps
-    swaps = build_heap(data)
+    swaps, elements_swaps = build_heap(data)
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-
+    print(elements_swaps)
 
     # output all swaps
     print(len(swaps))
