d2e1807b8d111f6ae656dd49b5d1e5e3b7e6e03e
Anastasija Bondare <125151183+221RDB395@users.noreply.github.com>
2023-03-18 17:18:57 +0000

    parbaude2

--- build_heap.py
+++ build_heap.py
@@ -4,65 +4,47 @@
 def build_heap(data):
     n = len(data)
     swaps = []
-    elements_swaps = 0
+
     for i in range(n // 2, -1, -1):
         k = i
         while 2 * k + 1 < n:
             j = 2 * k + 1
-            if j + 1 < n and data[j + 1] < data[j]:
-                j += 1
-            if data[k] <= data[j]:
+            if j + 1 < n:
+                if data[j + 1] < data[j]:
+                    j += 1
+            if data[k] > data[j]:
+                swaps.append((k, j))
+                data[k], data[j] = data[j], data[k]
+            else:
                 break
-            swaps.append((k, j))
-            data[k], data[j] = data[j], data[k]
             k = j
-            elements_swaps += 1
-    return swaps, elements_swaps 
+    return swaps
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
 
     ievade = input()
 
     if "I" in ievade: # Ja ievadītajā tekstā ir "I", tad 
-        number = int(input()) # ievada elementu skaitu, cik  būs kokā
-        data = list(map(int, input().split()))
-        assert len(data) == number    
+        n = int(input()) # ievada elementu skaitu, cik  būs kokā.
+        data = list(map(int, input().split())) # Ievadītajā skaitļu virknē atdala katru elementu ar vienu atstarpi.
+        assert len(data) == n # Pārbauda, vai ievadītais skaitlis sakrīt ar elementu skaitu virknē.
 
     if "F" in ievade: # Ja ievadītajā tekstā ir "F", tad 
         ievade = "tests/" + input() # nolasa visu ievadīto ceļu līdz failam,
         with open(ievade, 'r') as file: # nolasa faila saturu un aizver to,
-            number = int(file.readline().strip())  # pēc tam nolasa pirmo rindu un pārveido to par Integer jeb skaitli, kas norāda, cik elemntu būs kokā
-            data = list(map(int, file.readline().strip().split()))
-            assert len(data) == number
+            n = int(file.readline().strip())  # pēc tam nolasa pirmo rindu un pārveido to par Integer jeb skaitli, kas norāda, cik elemntu būs kokā
+            data = list(map(int, file.readline().strip().split())) # Lasa nākamo rindu, atdala elementus sava starpā ar vienu atstarpi un pārveido katru elementu par Integer skaitli.
+            # Mainīgajā "data" tiek piešķirta vērtība, kas saturēs n elementus no faila.
+            assert len(data) == n # Pārbauda, vai ievadītais skaitlis sakrīt ar elementu skaitu virknē.
 
-    if not ievade: # Ja ievade ir tukša, tad izvada kļūdas ziņojumu
-        print("Kļūdas ziņojums: ievade nevar būt tukša!")
-        return
-    
-    #n = int(input()) # input from keyboard
-    #data = list(map(int, input().split()))
+    swaps = build_heap(data) # Tiek izveidots maksimālais koks no mainīgā "data" elementiem,
+    # tas nozīmē, ka vislielākais elements kļūst par koka sakni un nākāmais lielākais elements tiks novietots koka otrajā līmenī.
+    # Un ar komandu swaps notiks elementu maiņa koka struktūrā.
 
-    # checks if lenght of data is the same as the said lenght
-    #assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps, elements_swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-    #print(elements_swaps)
-
-    # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
+    print(len(swaps)) # Izdrukā skaitu, cik reižu tika apmaninīti elementi ar vietām.
+    for i, j in swaps: 
+        print(i, j) # Tiek izvadīti divi skaitļi, kurus samainīja vietām.
 
 if __name__ == "__main__":
     main()
