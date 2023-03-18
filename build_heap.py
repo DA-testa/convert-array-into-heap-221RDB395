5e570e2f8ae81345ad89a453e845cdb24e5a6d20
Anastasija Bondare <125151183+221RDB395@users.noreply.github.com>
2023-03-18 18:34:55 +0000

    3

--- build_heap.py
+++ build_heap.py
@@ -2,30 +2,50 @@
 # python3
 
 def build_heap(data):
-    n = len(data)
+    n = len(data) # Jauns mainīgais n, kas satur saraksta "data" garumu
     swaps = []
 
     for i in range(n // 2, -1, -1):
-        k = i
-        while 2 * k + 1 < n:
-            j = 2 * k + 1
-            if j + 1 < n:
-                if data[j + 1] < data[j]:
-                    j += 1
-            if data[k] > data[j]:
-                swaps.append((k, j))
-                data[k], data[j] = data[j], data[k]
-            else:
-                break
-            k = j
-    return swaps
+        smallest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
 
+        if left < n and data[left] < data[smallest]:
+            smallest = left
+
+        if right < n and data[right] < data[smallest]:
+            smallest = right
+
+        if smallest != i:
+            swaps.append((i, smallest))
+            data[i], data[smallest] = data[smallest], data[i]
+            s = smallest
+            k = s
+
+            while 2 * s + 2 < n:
+                if data[2 * s + 2] < data[k]:
+                    k = 2 * s + 2
+                if data[2 * s + 1] < data[k]:
+                    k = 2 * s + 1
+                if k != s:
+                    swaps.append((s, k))
+                    data[s], data[k] = data[k], data[s]
+                    s = k
+                else:
+                    break
+
+            if 2 * s + 1 < n and data[2 * s + 1] < data[k]:
+                k = 2 * s + 1
+                swaps.append((s, k))
+                data[s], data[k] = data[k], data[s]
+
+    return swaps
 
 def main():
 
     ievade = input()
-
-    if "I" in ievade: # Ja ievadītajā tekstā ir "I", tad 
+    
+    if "I" in ievade: # Ja ievadītajā tekstā ir "i", tad 
         n = int(input()) # ievada elementu skaitu, cik  būs kokā.
         data = list(map(int, input().split())) # Ievadītajā skaitļu virknē atdala katru elementu ar vienu atstarpi.
         assert len(data) == n # Pārbauda, vai ievadītais skaitlis sakrīt ar elementu skaitu virknē.
@@ -43,7 +63,7 @@ def main():
     # Un ar komandu swaps notiks elementu maiņa koka struktūrā.
 
     print(len(swaps)) # Izdrukā skaitu, cik reižu tika apmaninīti elementi ar vietām.
-    for i, j in swaps: 
+    for i, j in swaps:
         print(i, j) # Tiek izvadīti divi skaitļi, kurus samainīja vietām.
 
 if __name__ == "__main__":
